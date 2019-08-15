eef2fe348d212ab5f4004cd4959aa3c455c0dbe6
Lassi Palojärvi <lassi.palojarvi@outlook.com>
2019-08-15 22:42:53 +0300

    2.20 fixed some catch notifications

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,152 +1,161 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 import Persons from "./components/Persons"
-import Form from './components/AddPersonForm'
-import Filter from './components/Filter'
-import PersonService from './services/Persons'
-import Message from './components/Message'
+import Form from "./components/AddPersonForm"
+import Filter from "./components/Filter"
+import PersonService from "./services/Persons"
+import Message from "./components/Message"
 
 const App = () => {
+	const [persons, setPersons] = useState([])
+	const [newName, setNewName] = useState("")
+	const [newNumber, setNewNumber] = useState("")
+	const [filter, setFilter] = useState("")
+	const [message, setMessage] = useState(null)
 
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
-  const [message, setMessage] = useState(null)
+	useEffect(() => {
+		PersonService.getAll().then(data => {
+			console.log(data)
+			setPersons(data)
+		})
 
-  useEffect(() => {
-    PersonService.getAll()
-      .then(data => {
-        console.log(data)
-        setPersons(data)
-      })
+		PersonService.getAll().then(res => console.log("service=", res))
+	}, [])
 
-    PersonService.getAll().then(res => console.log("service=", res))
-  }, [])
+	const nameChangeHandler = event => {
+		setNewName(event.target.value)
+	}
 
+	const deleteHandler = id => {
+		console.log("delete id =>", id)
 
-  const nameChangeHandler = (event) => {
-    setNewName(event.target.value)
-  }
+		let result = window.confirm(
+			"Do you want to delete message nr " + id + "."
+		)
+		if (!result) return
 
+		PersonService.deletePerson(id)
+			.then(res => {
+				console.log("delete res=>", res)
+				setPersons(persons.filter(p => p.id !== id))
 
-  const deleteHandler = (id) => {
-    console.log("delete id =>", id)
+				printMessage(`person id ${id}  deleted`)
+			})
+			.catch(() => {
+				printMessage(`Error: Deleting person id ${id} failed`)
+			})
+	}
 
-    let result = window.confirm("Do you want to delete message nr" + id)
-    if (!result)
-      return
+	const printMessage = message => {
+		setMessage(message)
+		resetMessage()
+	}
 
-    PersonService.deletePerson(id).then((res) => {
+	const numberChangeHandler = event => {
+		setNewNumber(event.target.value)
+	}
 
-      console.log("delete res=>", res)
-      setPersons(persons.filter(p => p.id !== id))
+	const addNewPerson = event => {
+		event.preventDefault()
 
-      printMessage(`person id ${id}  deleted`)
-    }).catch(() => {
-      printMessage(`Error: person id ${id} failed`)
-    })
-  }
+		const personObject = {
+			name: newName,
+			number: newNumber
+		}
 
-  const printMessage = (message) => {
-    setMessage(message)
-    resetMessage()
-  }
+		let pid = checkIfExist()
 
-  const numberChangeHandler = (event) => {
-    setNewNumber(event.target.value)
-  }
+		if (pid !== -1) {
+			if (
+				!window.confirm(
+					"Phonenumber already exists. Do you want to replace existing number with a new one?"
+				)
+			)
+				return
 
-  const addNewPerson = (event) => {
-    event.preventDefault()
+			PersonService.update(pid, personObject)
+				.then(() => {
+					setPersons(
+						persons.map(p => (p.id !== pid ? p : personObject))
+					)
 
-    const personObject = {
-      name: newName,
-      number: newNumber
-    }
+					printMessage(`person ${personObject.name} Updated`)
+				})
+				.catch(() => {
+					printMessage(`Error: Updating person id ${pid} failed`)
+				})
+		} else {
+			PersonService.create(personObject)
+				.then(newPerson => {
+					console.log("creating new person", newPerson)
+					setPersons(persons.concat(newPerson))
+					printMessage(`person ${personObject.name} added`)
+				})
+				.catch(() => {
+					printMessage(`Error: adding new person failed`)
+				})
+			console.log("personobject: ", personObject)
+		}
 
-    let pid = checkIfExist()
+		resetFields()
+	}
 
-    if (pid !== -1) {
+	const resetMessage = () => {
+		setTimeout(() => {
+			setMessage(null)
+		}, 5000)
+	}
 
-      if (!window.confirm("phonenumber already exists. Do you want to replace existing number with a new one?"))
-        return
+	const resetFields = () => {
+		setNewName("")
+		setNewNumber("")
+	}
 
-      PersonService.update(pid, personObject).then(() => {
-        setPersons(persons.map(p => p.id !== pid ? p : personObject))
+	const filterHandler = event => {
+		console.log("filtering....")
+		setFilter(event.target.value.toLowerCase())
+	}
 
-        printMessage(`person ${personObject.name} Updated`)
-      })
-        .catch(() => {
-          printMessage(`Error: person id ${pid} failed`)
-        })
-    } else {
+	const checkIfExist = () => {
+		let filteredPersons = persons.filter(p => p.name === newName)
+		if (filteredPersons.length > 0) {
+			return filteredPersons[0].id
+		}
 
-      PersonService.create(personObject).then((newPerson) => {
-        console.log("creating new person", newPerson)
-        setPersons(persons.concat(newPerson))
-        printMessage(`person ${personObject.name} added`)
-    
-      }).catch(() => {
-        printMessage(`Error: adding new person failed`)
-      })
-      console.log("personobject: ", personObject)
-    }
+		return -1
+	}
 
-    resetFields()
-  }
+	const getFilteredValues = () => {
+		return filter.length > 0
+			? persons.filter(p => p.name.toLowerCase().startsWith(filter))
+			: persons
+	}
 
-  const resetMessage = () => {
-    setTimeout(() => {
-      setMessage(null)
-    }, 5000)
-  }
-
-  const resetFields = () => {
-    setNewName('')
-    setNewNumber('')
-  }
-
-  const filterHandler = (event) => {
-    console.log("filtering....")
-    setFilter(event.target.value.toLowerCase())
-  }
-
-  const checkIfExist = () => {
-    let filteredPersons = persons.filter((p) => p.name === newName)
-    if (filteredPersons.length > 0) {
-      return filteredPersons[0].id
-    }
-
-    return -1
-  }
-
-  const getFilteredValues = () => {
-    return filter.length > 0 ? persons.filter(p => p.name.toLowerCase().startsWith(filter)) : persons
-  }
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Message message={message} />
-      <Filter data={{ filterHandler }} />
-
-      <h2>Add a new number</h2>
-
-      <Form data={{
-        addNewPerson,
-        newName,
-        nameChangeHandler,
-        newNumber,
-        numberChangeHandler
-      }} />
-
-      <h2>Numbers</h2>
-      <Persons values={getFilteredValues()} deleteHandler={deleteHandler} />
-
-    </div>
-  )
-
+	return (
+		<div>
+			<h2> Phonebook </h2> <Message message={message} />{" "}
+			<Filter
+				data={{
+					filterHandler
+				}}
+			/>
+			<h2> Add a new number </h2>
+			<Form
+				data={{
+					addNewPerson,
+					newName,
+					nameChangeHandler,
+					newNumber,
+					numberChangeHandler
+				}}
+			/>
+			<h2> Numbers </h2>{" "}
+			<Persons
+				values={getFilteredValues()}
+				deleteHandler={deleteHandler}
+			/>
+		</div>
+	)
 }
 
 export default App
